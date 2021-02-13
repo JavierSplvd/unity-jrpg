4d2d4cf31ec54f26f614ec233d30391a795196a7
Javier <takatsho@gmail.com>
2021-02-13 21:04:59 +0100

    Target selection on a state.

---
+++ State/SelectOneTargetState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SelectOneTargetState : State<Battle>
+{
+    public LostState(Battle owner) : base(owner)
+    {
+
+    }
+
+    public override void Tick()
+    {
+
+    }
+
+    public override void OnStateEnter()
+    {
+
+    }
+
+    public override void OnStateExit()
+    {
+
+    }
+}
