e117a7e71a8dd82e15621e8571d606f5bb9b5c56
Javier <takatsho@gmail.com>
2021-03-11 21:32:38 +0100

    Git includes Assets folder.

---
+++ SuperTiled2Unity/Import/pokemon-style.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="pokemon-style" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="../../Spritesheets/pokemon-style.png" width="128" height="256"/>
+ <terraintypes>
+  <terrain name="Water" tile="0"/>
+ </terraintypes>
+ <tile id="65" terrain="0,0,0,"/>
+ <tile id="66" terrain="0,0,,0"/>
+ <tile id="73" terrain="0,,0,0"/>
+ <tile id="74" terrain=",0,0,0"/>
+ <tile id="80" terrain=",,,0"/>
+ <tile id="81" terrain=",,0,0"/>
+ <tile id="82" terrain=",,0,"/>
+ <tile id="88" terrain=",0,,0"/>
+ <tile id="89" terrain="0,0,0,0"/>
+ <tile id="90" terrain="0,,0,"/>
+ <tile id="96" terrain=",0,,"/>
+ <tile id="97" terrain="0,0,,"/>
+ <tile id="98" terrain="0,,,"/>
+</tileset>
