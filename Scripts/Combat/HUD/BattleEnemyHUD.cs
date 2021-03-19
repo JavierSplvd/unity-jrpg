7933895b849127d031574ce98180ec4a16d3f986
Javier <takatsho@gmail.com>
2021-03-19 11:17:38 +0100

    Status effect hud for enemies.

--- Scripts/Combat/HUD/BattleEnemyHUD.cs
+++ Scripts/Combat/HUD/BattleEnemyHUD.cs
@@ -1,56 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattleEnemyHUD : MonoBehaviour, IBattleHUD
-{
+public class BattleEnemyHUD : MonoBehaviour, IBattleHUD {
     [SerializeField] private UnitSO unit;
     [SerializeField] private Image image;
     private SkillAnimation skillAnimation;
     public DamageLogText damageLog;
+    public StatusEffectsHUD statusEffects;
 
-    void Awake()
-    {
+    void Awake() {
         int childCount = transform.childCount;
-        for(int i = 0; i < childCount; i++)
-        {
+        for (int i = 0; i < childCount; i++) {
             Transform c = transform.GetChild(i);
-            if(c.name.Equals("Image"))
-            {
+            if (c.name.Equals("Image")) {
                 image = c.gameObject.GetComponent<Image>();
-            }
-            else if(c.name.Equals("SkillAnimation"))
-            {
+            } else if (c.name.Equals("SkillAnimation")) {
                 skillAnimation = c.gameObject.GetComponent<SkillAnimation>();
-            }
-            else if(c.name.Equals("DamageLog"))
-            {
+            } else if (c.name.Equals("DamageLog")) {
                 damageLog = c.gameObject.GetComponent<DamageLogText>();
             }
+            statusEffects = transform.GetComponentInChildren<StatusEffectsHUD>();
         }
     }
 
-    void FixedUpdate()
-    {
-        if(unit == null)
-        {
+    void FixedUpdate() {
+        if (unit == null) {
             // Destroy(this);
             throw new System.Exception("Unit is null. This component is destroyed.");
         }
     }
 
-    public void SetUnit(UnitSO unit)
-    {
+    public void SetUnit(UnitSO unit) {
         this.unit = unit;
         skillAnimation.SetUnitId(unit.unitId);
         damageLog.SetId(unit.unitId);
         image.sprite = unit.sprite;
         image.rectTransform.sizeDelta = new Vector2(unit.sprite.rect.width, unit.sprite.rect.height);
+        statusEffects.Init(unit);
     }
 
-    public void UpdateData(UnitSO unit)
-    {
+    public void UpdateData(UnitSO unit) {
         throw new System.NotImplementedException();
     }
 
---
+++ SuperTiled2Unity/Import/b-desert.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="b-desert" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../Tilesets/b-desert.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="terrain-a" tile="324"/>
+  <terrain name="terrain-b" tile="324"/>
+  <terrain name="grass" tile="324"/>
+ </terraintypes>
+ <tile id="225" terrain="1,1,1,"/>
+ <tile id="226" terrain="1,1,,1"/>
+ <tile id="228" terrain="0,0,0,"/>
+ <tile id="229" terrain="0,0,,0"/>
+ <tile id="231" terrain="2,2,2,"/>
+ <tile id="232" terrain="2,2,,2"/>
+ <tile id="257" terrain="1,,1,1"/>
+ <tile id="258" terrain=",1,1,1"/>
+ <tile id="260" terrain="0,,0,0"/>
+ <tile id="261" terrain=",0,0,0"/>
+ <tile id="263" terrain="2,,2,2"/>
+ <tile id="264" terrain=",2,2,2"/>
+ <tile id="288" terrain=",,,1"/>
+ <tile id="289" terrain=",,1,1"/>
+ <tile id="290" terrain=",,1,"/>
+ <tile id="291" terrain=",,,0"/>
+ <tile id="292" terrain=",,0,0"/>
+ <tile id="293" terrain=",,0,"/>
+ <tile id="294" terrain=",,,2"/>
+ <tile id="295" terrain=",,2,2"/>
+ <tile id="296" terrain=",,2,"/>
+ <tile id="320" terrain=",1,,1"/>
+ <tile id="321" terrain="1,1,1,1"/>
+ <tile id="322" terrain="1,,1,"/>
+ <tile id="323" terrain=",0,,0"/>
+ <tile id="324" terrain="0,0,0,0"/>
+ <tile id="325" terrain="0,,0,"/>
+ <tile id="326" terrain=",2,,2"/>
+ <tile id="327" terrain="2,2,2,2"/>
+ <tile id="328" terrain="2,,2,"/>
+ <tile id="352" terrain=",1,,"/>
+ <tile id="353" terrain="1,1,,"/>
+ <tile id="354" terrain="1,,,"/>
+ <tile id="355" terrain=",0,,"/>
+ <tile id="356" terrain="0,0,,"/>
+ <tile id="357" terrain="0,,,"/>
+ <tile id="358" terrain=",2,,"/>
+ <tile id="359" terrain="2,2,,"/>
+ <tile id="360" terrain="2,,,"/>
+</tileset>
